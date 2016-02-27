011afb24f7e2033cf4e9c7aa22963022d0eec4d8
Andy Taylor <Theador0691@gmail.com>
2016-02-27 17:29:09 +0000

    changed routes to put index as first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
   get 'static_pages/about'
 
   get 'static_pages/price'
 
   get 'static_pages/contact'
 
-  get 'static_pages/index'
+  root 'static_pages/index'
 
   post 'static_pages/thank_you'
 
